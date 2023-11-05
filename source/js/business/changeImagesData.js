e25153d7d412276a5290b83df157610135fd595a
restby <restby@gmail.com>
2023-11-05 07:23:49 +0300

    ничего важного

--- source/js/business/changeImagesData.js
+++ source/js/business/changeImagesData.js
@@ -5,19 +5,22 @@ const changeImagesData = (itemsObject, itemsObjectKey) => {
     const buttons = document.querySelectorAll('[data-button-value]');
 
     const itemImageAltTextChapter = {
-      urlNoWheel: 'фото без колес',
-      urlNoWheelH300: 'фото без колес с тентом H300',
-      urlNoWheelH600: 'фото без колес с тентом H600',
-      urlNoWheelH1220: 'фото без колес с тентом H1220',
-      urlNoWheelH1220C: 'фото без колес с тентом H1220C',
-      urlWheel2R13: 'фото с 2 колесами R13',
-      urlWheel2R13H300: 'фото с 2 колесами R13 и с тентом H300',
-      urlWheel2R13H600: 'фото без колес с тентом H1220C',
-      urlWheel2R13H1220: 'фото с 2 колесами R13 и с тентом H1220',
-      urlWheel2R13H1220C: 'фото с 2 колесами R13 и с тентом H1220C',
-      urlWheel4R13: 'фото с 4 колесами R13',
-      urlWheel4R13H1220: 'фото с 4 колесами R13 и с тентом H1220',
-      urlWheel4R13H1220C: 'фото с 4 колесами R13 и с тентом H1220C',
+      urlNo2Wheel: 'фото без колес 1 ось',
+      urlNo2WheelH300: 'фото без колес 1 осьс тентом H300',
+      urlNo2WheelH600: 'фото без колес 1 ось с тентом H600',
+      urlNo2WheelH1220: 'фото без колес 1 ось с тентом H1220',
+      urlNo2WheelH1220C: 'фото без колес 1 ось с тентом H1220C',
+      urlNo4Wheel: 'фото без колес 2 оси',
+      urlNo4WheelH1220: 'фото без колес 2 оси с тентом H1220',
+      urlNo4WheelH1220C: 'фото без колес 2 оси с тентом H1220C',
+      urlWheel2R13: 'фото с 2 колесами R13 1 ось',
+      urlWheel2R13H300: 'фото с 2 колесами R13 1 ось и с тентом H300',
+      urlWheel2R13H600: 'фото с 2 колесами R13 1 ось с тентом H600',
+      urlWheel2R13H1220: 'фото с 2 колесами R13 1 ось и с тентом H1220',
+      urlWheel2R13H1220C: 'фото с 2 колесами R13 1 ось и с тентом H1220C',
+      urlWheel4R13: 'фото с 4 колесами R13 2 оси',
+      urlWheel4R13H1220: 'фото с 4 колесами R13 2 оси и с тентом H1220',
+      urlWheel4R13H1220C: 'фото с 4 колесами R13 2 оси и с тентом H1220C',
     };
 
     buttons.forEach((button) => {
